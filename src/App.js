5a1b0e0771185f70680150bbfd4e4186b529ce16
ericamarghescu <erica.marghescu@gmail.com>
2022-05-08 07:00:04 -0400

    added skills page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './Components/Navbar.js';
+import Homepage from './Components/Homepage.js'
+import Section1 from './Components/Section1.js';
+import Section2 from './Components/Section2.js';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div className="app">
+        <Navbar/>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Homepage/>} />
+            <Route path='/Section2' element={<Section2/>} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
