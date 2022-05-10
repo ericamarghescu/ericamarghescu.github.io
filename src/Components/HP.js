f7ff456670d8a3c25564257d35fb4b045ad796ca
ericamarghescu <erica.marghescu@gmail.com>
2022-05-10 15:56:31 -0400

    changed animations

--- src/Components/HP.js
+++ src/Components/HP.js
@@ -1,8 +1,9 @@
 import React from "react";
-import './Section2.css'
+import arrow from './arrow.png'
 import backarrow from './backarrow.png'
 import {Link } from "react-router-dom";
-import './HP.css'
+import './HP.css';
+import './Section3BG.css'
 import hp1 from './hp1.png'
 import hp2 from './hp2.png'
 
@@ -21,36 +22,72 @@ export default class HP extends React.Component {
     return (
       <div className="HP"> 
          <div className= "PanelHP">
-            <div className="HP-BoxHP">
-               <Link className="LinkHP" to="/Projects">
-                  <img className="arrowHP" src={backarrow}/>
+         <Link className="Linkbg" to="/Skills">
+               <img className="backarrow3bg" src={backarrow}/>
+           </Link>
+            <h1 className="Projectsbg">P R O J E C T S</h1>
+            <div className="RF-Boxbg">
+               <div className="RF-Titleboxbg">
+                  <h1 className="Titlebg">Recipe Finder</h1>
+                  <h2 className="Stampbg">Technica Hackathon 2021</h2>
+               </div>
+               <Link className="Linkprojbg" to="/Recipe-Finder">
+                  <img className="arrowprojbg" src={arrow}/>
                </Link>
-               <div className="HP-TitleboxHP">
-                  <h1 className="TitleHP">H O N E Y P O T</h1>
-                  <h1 className="TitleHP">E X P E R I M E N T</h1>
-                  <h2 className="StampHP">University of Maryland 2021</h2>
+            </div>
+            <div className="HP-Boxbg">
+               <div className="HP-Titleboxbg">
+                  <h1 className="Titlebg">Honeypot Experiment</h1>
+                  <h2 className="Stampbg">University of Maryland</h2>
                </div>
-               <div className="hpdisplaybackground">
-                  <img className="hpdisplay" src={hp1} />
+               <Link className="Linkprojbg" to="/Honeypot-Experiment">
+                  <img className="arrowprojbg" src={arrow}/>
+               </Link>
+            </div>
+            <div className="Sudoku-Boxbg">
+               <div className="Sudoku-Titleboxbg">
+                  <h1 className="Titlebg">Sudoku App</h1>
+                  <h2 className="Stampbg">Personal Project</h2>
                </div>
-               <div className="hppopupbackground">
-                  <img className="hppopup" src={hp2} />
-               </div>
-               <div className="hpdescbg">
-                  <div className="hpdesc">
-                     <h1 className="hp-desc-title">D E S C R I P T I O N</h1>
-                     <p className="hp-desc">Experiment designed to determine how the delay in ssh login time can impact
-                        an attacker's behavior. Multiple test honeypots were created inside containers with tools installed such as 
-                        MITM and SnoopyLogger to monitor attacker behavior.</p>
+               <Link className="Linkprojbg" to="/Sudoku">
+                  <img className="arrowprojbg" src={arrow}/>
+               </Link>
+            </div>
+            <Link className="Linkbg" to="/Coursework">
+               <img className="arrow3bg" src={arrow}/>
+           </Link>
+            <div className="Panel-layerHP">
+               <div className="HP-BoxHP">
+                  <Link className="LinkHP" to="/Projects">
+                     <img className="arrowHP" src={backarrow}/>
+                  </Link>
+                  <div className="HP-TitleboxHP">
+                     <h1 className="TitleHP">H O N E Y P O T</h1>
+                     <h1 className="TitleHP">E X P E R I M E N T</h1>
+                     <h2 className="StampHP">University of Maryland 2021</h2>
                   </div>
-               </div>
-               <div className="hptechbg">
-                  <div className="hptech">
-                     <h1 className="hp-desc-title">T E C H N O L O G I E S</h1>
-                     <div className="hptech-box">Bash Scripts</div>
-                     <div className="hptech-box">Python</div>
-                     <div className="hptech-box">MITM</div>
-                     <div className="hptech-box">SnoopyLogger</div>
+                  <div className="hpdisplaybackground">
+                     <img className="hpdisplay" src={hp1} />
+                  </div>
+                  <div className="hppopupbackground">
+                     <img className="hppopup" src={hp2} />
+                  </div>
+                  <div className="hpdescbg">
+                     <div className="hpdesc">
+                        <h1 className="hp-desc-title">D E S C R I P T I O N</h1>
+                        <p className="hp-desc">Experiment designed to determine how the delay in ssh login time can impact
+                           an attacker's behavior. Multiple test honeypots were created inside containers with tools installed such as 
+                           MITM and SnoopyLogger to monitor attacker behavior.</p>
+                     </div>
+                  </div>
+                  <div className="hptechbg">
+                     <div className="hptech">
+                        <h1 className="hp-desc-title">T E C H N O L O G I E S</h1>
+                        <div className="hptech-box">Bash Scripts</div>
+                        <div className="hptech-box">Python</div>
+                        <div className="hptech-box">MITM</div>
+                        <div className="hptech-box">SnoopyLogger</div>
+                     </div>
                   </div>
                </div>
             </div>
--- src/Components/RF.js
+++ src/Components/RF.js
@@ -1,10 +1,11 @@
 import React from "react";
-import './Section2.css'
-import backarrow from './backarrow.png'
+import arrow from './arrow.png';
+import backarrow from './backarrow.png';
 import {Link } from "react-router-dom";
-import './RF.css'
-import rfdisplay from './rfdisplay.png'
-import rfpopup from './rfpopup.png'
+import './RF.css';
+import './Section3BG.css';
+import rfdisplay from './rfdisplay.png';
+import rfpopup from './rfpopup.png';
 
 export default class RF extends React.Component {
 
@@ -21,36 +22,72 @@ export default class RF extends React.Component {
     return (
       <div className="RF"> 
          <div className= "PanelRF">
-            <div className="RF-BoxRF">
-               <Link className="LinkRF" to="/Projects">
-                  <img className="arrowRF" src={backarrow}/>
+         <Link className="Linkbg" to="/Skills">
+               <img className="backarrow3bg" src={backarrow}/>
+           </Link>
+            <h1 className="Projectsbg">P R O J E C T S</h1>
+            <div className="RF-Boxbg">
+               <div className="RF-Titleboxbg">
+                  <h1 className="Titlebg">Recipe Finder</h1>
+                  <h2 className="Stampbg">Technica Hackathon 2021</h2>
+               </div>
+               <Link className="Linkprojbg" to="/Recipe-Finder">
+                  <img className="arrowprojbg" src={arrow}/>
                </Link>
-               <div className="RF-TitleboxRF">
-                  <h1 className="TitleRF">R E C I P E</h1>
-                  <h1 className="TitleRF">F I N D E R</h1>
-                  <h2 className="StampRF">Technica Hackathon 2021</h2>
+            </div>
+            <div className="HP-Boxbg">
+               <div className="HP-Titleboxbg">
+                  <h1 className="Titlebg">Honeypot Experiment</h1>
+                  <h2 className="Stampbg">University of Maryland</h2>
                </div>
-               <div className="rfdisplaybackground">
-                  <img className="rfdisplay" src={rfdisplay} />
+               <Link className="Linkprojbg" to="/Honeypot-Experiment">
+                  <img className="arrowprojbg" src={arrow}/>
+               </Link>
+            </div>
+            <div className="Sudoku-Boxbg">
+               <div className="Sudoku-Titleboxbg">
+                  <h1 className="Titlebg">Sudoku App</h1>
+                  <h2 className="Stampbg">Personal Project</h2>
                </div>
-               <div className="rfpopupbackground">
-                  <img className="rfpopup" src={rfpopup} />
-               </div>
-               <div className="rfdescbg">
-                  <div className="rfdesc">
-                     <h1 className="desc-title">D E S C R I P T I O N</h1>
-                     <p className="award">Winner of M&#38;T Bank's Best Hack for Influencing Human Behavior at Home</p>
-                     <p className="desc">Website designed to take user's dietary preferences as input and generate either
-                        a list of possible recipes of interest, or a meal plan for the week.</p>
+               <Link className="Linkprojbg" to="/Sudoku">
+                  <img className="arrowprojbg" src={arrow}/>
+               </Link>
+            </div>
+            <Link className="Linkbg" to="/Coursework">
+               <img className="arrow3bg" src={arrow}/>
+           </Link>
+            <div className="Panel-layerRF">
+               <div className="RF-BoxRF">
+                  <Link className="LinkRF" to="/Projects">
+                     <img className="arrowRF" src={backarrow}/>
+                  </Link>
+                  <div className="RF-TitleboxRF">
+                     <h1 className="TitleRF">R E C I P E</h1>
+                     <h1 className="TitleRF">F I N D E R</h1>
+                     <h2 className="StampRF">Technica Hackathon 2021</h2>
                   </div>
-               </div>
-               <div className="rftechbg">
-                  <div className="rftech">
-                     <h1 className="desc-title">T E C H N O L O G I E S</h1>
-                     <div className="tech-box">React</div>
-                     <div className="tech-box">JSX</div>
-                     <div className="tech-box">CSS</div>
-                     <div className="tech-box">Spoonacular API</div>
+                  <div className="rfdisplaybackground">
+                     <img className="rfdisplay" src={rfdisplay} />
+                  </div>
+                  <div className="rfpopupbackground">
+                     <img className="rfpopup" src={rfpopup} />
+                  </div>
+                  <div className="rfdescbg">
+                     <div className="rfdesc">
+                        <h1 className="desc-title">D E S C R I P T I O N</h1>
+                        <p className="award">Winner of M&#38;T Bank's Best Hack for Influencing Human Behavior at Home</p>
+                        <p className="desc">Website designed to take user's dietary preferences as input and generate either
+                           a list of possible recipes of interest, or a meal plan for the week.</p>
+                     </div>
+                  </div>
+                  <div className="rftechbg">
+                     <div className="rftech">
+                        <h1 className="desc-title">T E C H N O L O G I E S</h1>
+                        <div className="tech-box">React</div>
+                        <div className="tech-box">JSX</div>
+                        <div className="tech-box">CSS</div>
+                        <div className="tech-box">Spoonacular API</div>
+                     </div>
                   </div>
                </div>
             </div>
